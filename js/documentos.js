4ceeca033a7460aa9d6b036ef28a4a0099b15143
Stephanie Lima <slrocha17@gmail.com>
2020-01-29 20:57:47 -0300

    Adicionando linha no tag ul usando javascript

--- js/documentos.js
+++ js/documentos.js
@@ -7,6 +7,7 @@ function validation(cpfValue){
      /*Retorna uma promise que será processada*/
     storage.ref().child(cpfValue).listAll().then(function(allFiles){
         if(allFiles.items.length >=1){
+            listFiles(cpfValue);
             next(cpfValue);
         }else{
             alert('Cpf não encontrado');
@@ -16,12 +17,41 @@ function validation(cpfValue){
     });     
 }
 
+function listFiles(cpfValue){
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
+    var storage = firebase.storage();
+    var fileNames = [];
+    var fileLinks = [];
+    var files;
+    storage.ref().child(cpfValue).listAll().then(function(allFiles){
+        files = allFiles.items;
+        console.log(files);
+        for(let i=0; i<files.length; i++){
+            fileNames.push(files[i].name);
+            storage.ref(cpfValue+'/'+fileNames[i]).getDownloadURL().then(function(url){
+                //console.log(url);
+                var ul = document.getElementById("list");
+                var li = document.createElement("li");
+                var listItems = "<a href = '"+url+"' target='_blank'>"+fileNames[i]+"</a>";
+                li.innerHTML = listItems;
+                ul.appendChild(li);
+                fileLinks.push(url);
+            }).catch(function(error){
+                console.log(error);
+            }).finally(function(){
+                console.log(fileNames[i]);
+                console.log(fileLinks[i]);
+                
+            });       
+         }
+    });
+}
+
 function next(cpfValue){
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
-
 }
+
 function back(){
     document.getElementById('busca').removeAttribute("class", "ocultar");
     document.getElementById('resultado').setAttribute("class", "ocultar");
